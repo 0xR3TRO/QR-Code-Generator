bb921cbfa9bb8c623d459a00a0e9eaf9ce3e6954
0xR3TR0 <oliwier@klefas.com>
2023-11-02 22:59:55 +0100

    0xR3TRO

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Scripts and functions on this page are the creation of 0xR3TR0. All rights reserved.
+// Scripts and functions on this page are the creation of 0xR3TR0. All rights reserved. 
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
